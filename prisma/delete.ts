bc050d82716d217a4b502db7a5b43435eddb38a5
Eklavya Nath <optimusprime172004@gmail.com>
2025-10-12 17:44:00 +0530

    deleteUser ids

--- prisma/delete.ts
+++ prisma/delete.ts
@@ -65,7 +65,7 @@ async function getUser(userId: string) {
   }
 }
 
-const deleteUserIds = [""];
+const deleteUserIds = ["558f2e65-84f6-48d2-9fda-4e787dff2d70","53754466-b037-4439-8e96-dff41be408f8","5283cb36-c4a2-45b3-920f-d3afd02c8553","4e26bbaf-9e48-420c-b83d-89a2008133f0","3ad2b96a-333c-4fad-b8bf-7f1bd503189c","37426ff7-6f9c-4837-9222-a0db6f23483c","2f64e0e5-2e19-4061-b176-104edf9448f8","2506519c-c309-4534-a8ac-75b20b2e75dc","0da8c3d6-ef0d-4805-b27c-9387366e0773","013945ea-9138-4034-9b4f-d503675968a3"];
 
 deleteUserIds.forEach((userId) => {
   deleteUser(userId)
